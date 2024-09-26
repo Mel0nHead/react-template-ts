9558cf5b7ad420869c17822235cad65ab3aa484a
Alex King <48763366+Mel0nHead@users.noreply.github.com>
2024-09-26 07:32:18 +0100

    Better handling of fetch status

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,56 @@
 import { useEffect, useState } from "react";
 
+enum FetchStatus {
+  Idle,
+  Loading,
+  Success,
+  Error,
+}
+
 function App() {
   const [expensesData, setExpensesData] = useState(null);
+  const [fetchStatus, setFetchStatus] = useState<FetchStatus>(FetchStatus.Idle);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(
-        "https://expenses-backend-mu.vercel.app/expenses",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Username: "Mark.Marks", // <--- TODO: use your name
-          },
-        }
-      );
+      setFetchStatus(FetchStatus.Loading);
 
-      const data = await response.json();
-      setExpensesData(data);
+      try {
+        const response = await fetch(
+          "https://expenses-backend-mu.vercel.app/expenses",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Username: "Mark.Marks", // <--- TODO: use your name
+            },
+          }
+        );
+
+        const data = await response.json();
+        setExpensesData(data);
+        setFetchStatus(FetchStatus.Success);
+      } catch (e) {
+        setFetchStatus(FetchStatus.Error);
+        console.log(e);
+      }
     }
 
     fetchData();
   }, []);
 
-  return <div>{expensesData ? JSON.stringify(expensesData) : null}</div>;
+  if (fetchStatus === FetchStatus.Loading || fetchStatus === FetchStatus.Idle) {
+    return <b>Loading...</b>;
+  }
+
+  if (fetchStatus === FetchStatus.Error) {
+    return <b>An error occurred. Please refresh the page and try again.</b>;
+  }
+
+  return (
+    <div>
+      {expensesData ? JSON.stringify(expensesData) : <b>No data was found</b>}
+    </div>
+  );
 }
 
 export default App;
